5cc288c9e57cd598b6ed0b4a9d9dc7ad9b6d639b
Vladyslav Valeev <vladyslav.v@blaize.tech>
2022-06-17 14:20:31 +0300

    One more

--- src/lib.rs
+++ src/lib.rs
@@ -81,7 +81,7 @@ pub fn get_eth_address(address: String) -> EthAddress {
     assert_eq!(data.len(), 20, "address should be 20 bytes long");
     let mut result = [0u8; 20];
     result.copy_from_slice(&data);
-    let i: String; ///
+    let i: String; //
     result
 }
 
