2a2c2150d2e6a0a608b690daacbbfed1f43cf3f1
Vladyslav Valeev <vladyslav.v@blaize.tech>
2022-06-20 17:30:12 +0300

    One more

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,7 @@ pub fn process_instruction(
     greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
 
     msg!("Greeted {} time(s)!", greeting_account.counter);
-    // 34drv
+    // 34d
     Ok(())
 }
 
